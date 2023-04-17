4d3de67319e721a88e66635c5874ade81c3b1bb0
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-17 11:21:11 +0700

    init entity

---
+++ app/entity/destinationProduct.go
@@ -0,0 +1,17 @@
+package entity
+
+import "time"
+
+type DestinationProduct struct {
+	ID           uint64    `gorm:"primaryKey" json:"id"`
+	ProductName  string    `gorm:"not null" json:"product_name"`
+	Qty          uint64    `gorm:"not null;default:1" json:"qty" `
+	SellingPrice uint64    `json:"selling_price" `
+	PromoPrice   uint64    `json:"promo_price" `
+	CreatedAt    time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+}
+
+func (DestinationProduct) TableName() string {
+	return "destination_product"
+}
---
+++ app/entity/sourceProduct.go
@@ -0,0 +1,17 @@
+package entity
+
+import "time"
+
+type SourceProduct struct {
+	ID           uint64    `gorm:"primaryKey" json:"id"`
+	ProductName  string    `gorm:"not null" json:"product_name"`
+	Qty          uint64    `gorm:"not null;default:1" json:"qty" `
+	SellingPrice uint64    `json:"selling_price" `
+	PromoPrice   uint64    `json:"promo_price" `
+	CreatedAt    time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+}
+
+func (SourceProduct) TableName() string {
+	return "source_product"
+}
