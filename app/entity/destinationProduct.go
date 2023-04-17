baccb24f8878660026d4f06b0b66ca8da3c96dfa
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-17 12:38:50 +0700

    fix gorm struct

--- app/entity/destinationProduct.go
+++ app/entity/destinationProduct.go
@@ -5,7 +5,7 @@ import "time"
 type DestinationProduct struct {
 	ID           uint64    `gorm:"primaryKey" json:"id"`
 	ProductName  string    `gorm:"not null" json:"product_name"`
-	Qty          uint64    `gorm:"not null;default:1" json:"qty" `
+	Qty          uint64    `json:"qty" `
 	SellingPrice uint64    `json:"selling_price" `
 	PromoPrice   uint64    `json:"promo_price" `
 	CreatedAt    time.Time `gorm:"autoCreateTime" json:"createdAt"`
